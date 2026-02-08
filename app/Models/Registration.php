4e77027518182a813fdd44afeb20d75b5de408a5
loki-099 <loidatolentino358@gmail.com>
2026-02-08 13:56:19 +0800

    define relationship of Event and Workshops

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Registration extends Model
 {
     //
+
+
+    
+    public const CREATED_AT = 'registered_date';
+    public const UPDATED_AT = 'updated_at';
 }
--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Workshop extends Model
 {
     //
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
